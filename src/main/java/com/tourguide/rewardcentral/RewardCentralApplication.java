7086b83c1d80e7580ededcfbe1e3d07e7b946d12
David <d6797125@pm.me>
2021-12-15 10:08:14 +0100

    refacto: update Main class capitalization

--- src/main/java/com/tourguide/rewardcentral/RewardCentralApplication.java
+++ src/main/java/com/tourguide/rewardcentral/RewardCentralApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RewardcentralApplication {
+public class RewardCentralApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RewardcentralApplication.class, args);
+        SpringApplication.run(RewardCentralApplication.class, args);
     }
 
 }
