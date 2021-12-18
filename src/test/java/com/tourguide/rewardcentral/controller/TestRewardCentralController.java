b61d7f69a2eafd6f900e689e8852be1a156dbb53
David <d6797125@pm.me>
2021-12-18 20:11:22 +0100

    test: add TestRewardCentralController

--- src/test/java/com/tourguide/rewardcentral/controller/TestRewardCentralController.java
+++ src/test/java/com/tourguide/rewardcentral/controller/TestRewardCentralController.java
@@ -1,10 +1,10 @@
-package com.tourguide.rewardcentral;
+package com.tourguide.rewardcentral.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RewardCentralApplicationTests {
+class TestRewardCentralController {
 
     @Test
     void contextLoads() {
